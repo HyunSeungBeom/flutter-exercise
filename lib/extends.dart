628d849ae5969a0a25559074eab166a8c414ef15
현승범 <ghfk100@naver.com>
2023-09-04 14:00:38 +0900

    extends

---
+++ lib/extends.dart
@@ -0,0 +1,34 @@
+class SuperClass {
+  String name = 'Super';
+
+  void run() {
+    print('superclass run');
+  }
+}
+
+class SubClass extends SuperClass {
+  String name = 'Sub';
+
+  @override
+  void run() {
+    super.run();
+    this.fly();
+  }
+
+  void fly() {
+    print('subclass fly');
+  }
+}
+
+void main() {
+  var superClass = SuperClass();
+  superClass.run();
+  print(superClass.name);
+
+  print('------------------');
+
+  var subClass = SubClass();
+  subClass.run();
+  subClass.fly();
+  print(subClass.name);
+}
